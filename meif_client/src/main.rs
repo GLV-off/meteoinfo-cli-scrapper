fd51542bf4fcd468b5ce7053f80d25be50b72ae8
GLV-off <glv.off@mail.ru>
2025-04-27 16:10:13 +0300

    Start

---
+++ meif_client/src/main.rs
@@ -0,0 +1,80 @@
+use std::io;
+
+use scraper::{
+    Html,
+    html::Select,
+    Selector
+};
+use ureq;
+
+fn get_content_file<C>(path: &str, on_error: C) -> String 
+    where C:FnOnce(io::Error)
+{
+    match std::fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(error) => {
+            on_error(error);
+            "error happened".to_string()
+        }
+    }
+}
+
+fn get_content_bytes(url: String) -> Result<Vec<u8>, ureq::Error>  {
+    let mut resp = ureq::get(url)
+        .call()?;
+
+    let cont = resp
+        .body_mut()
+        .read_to_vec()?;
+
+    Ok(cont)
+}
+
+fn get_content(url: String) -> Result<String, ureq::Error> {
+    let mut resp = ureq::get(url)
+        .call()?;
+
+    let cont = resp
+        .body_mut()
+        .read_to_string()?;
+
+    Ok(cont)
+}
+
+fn get_images_paths(sel: Select) -> Vec<String> {
+    sel.map(|x| 
+        x.value()
+            .attr("href")
+            .unwrap_or("")
+            .to_string()
+        )
+        .collect()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ureq::Error> {
+    let host = "https://meteoinfo.ru";
+    let url_images = String::from(host) + "/satellite-images";
+    const FILENAME: &str = "content.html";
+
+    let content = if std::fs::exists(FILENAME )? {
+        get_content_file(FILENAME, |e| println!("Error reading file: {}", e))
+    } else {
+        get_content(url_images)?
+    };
+       
+    let document = Html::parse_document(&content);
+    let sel = Selector::parse("a[href^='/images/media/satel/']")
+        .expect("Selector parsing failed!");    
+    let images_paths = get_images_paths(document.select(&sel));
+
+    for (index, image_path) in images_paths.iter().enumerate() {
+        println!("{}: {}", index, image_path);
+
+        let image_content = get_content_bytes(String::from(host) + image_path)?;
+
+        std::fs::write(format!("img_{}.gif", index), image_content)?;
+    }
+
+    Ok(())
+}
