c30c7a7dd4d6076ecb7717494f780a6428f4dba8
GLV-off <glv.off@mail.ru>
2025-05-09 23:54:52 +0300

    Formatting, clippy checks and fixing things with refactoring

--- meif_client/src/main.rs
+++ meif_client/src/main.rs
@@ -1,18 +1,14 @@
 use scraper::{Html, Selector, html::Select};
 
+use log::{error, info};
 use simplelog::{
-    LevelFilter,
-    CombinedLogger,
-    TermLogger,
+    ColorChoice, CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode,
     WriteLogger,
-    Config,
-    TerminalMode,
-    ColorChoice
 };
-use log::{info, error};
 
 const GLV_METEOINFO_HOST: &str = "";
-const FILENAME: &str = "content.html";
+const FILENAME_SYNAPT: &str = "content_synaptic.html";
+const FILENAME_IMAGES: &str = "content_images.html";
 const DEFAULT_LOG_FILENAME: &str = "meteo_log.log";
 
 #[tokio::main]
@@ -21,42 +17,49 @@ async fn main() -> Result<(), ureq::Error> {
 
     let host = get_host();
     let url_images = get_images_url(&host);
-    let content = get_content(url_images)?;
+    let content = get_content(url_images, FILENAME_IMAGES)?;
     let images_paths = parse_images_from_content(content);
-    process_images(host, images_paths)?;
+    process_images(&host, images_paths)?;
+
+    // GLV: Prototyping parsing synaptic map's
+    let url_synaptic = get_synaptic_url(&host);
+    let content = get_content(url_synaptic, FILENAME_SYNAPT)?;
+    std::fs::write(FILENAME_SYNAPT, content)?;
 
     Ok(())
 }
 
 fn setup_logging() {
-        // Настройка логирования по всему продукту
-    CombinedLogger::init(
-        vec![
-            TermLogger::new(LevelFilter::Info, 
-                Config::default(), 
-                TerminalMode::Mixed, 
-                ColorChoice::Auto),
-            WriteLogger::new(LevelFilter::Info, 
-                Config::default(), 
-                std::fs::File::create(DEFAULT_LOG_FILENAME).unwrap()),
-        ]
-    ).unwrap();
+    // Настройка логирования по всему продукту
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            std::fs::File::create(DEFAULT_LOG_FILENAME).unwrap(),
+        ),
+    ])
+    .unwrap();
 }
 
 fn get_host() -> String {
-    let host = match get_meteoinfo_host() {
+    match get_meteoinfo_host() {
         Ok(host) => host,
-        Err(error) => { 
+        Err(error) => {
             let def_host = "https://meteoinfo.ru";
             info!("Failed to read from environment host: {}", error);
             info!("Using default host: {}", def_host);
-            def_host.to_string() 
+            def_host.to_string()
         }
-    };
-    host
+    }
 }
 
-fn get_images_url(host:& String) -> String {
+fn get_images_url(host: &String) -> String {
     format!("{}/satellite-images", host)
 }
 
@@ -64,17 +67,20 @@ fn get_meteoinfo_host() -> Result<String, std::env::VarError> {
     std::env::var(GLV_METEOINFO_HOST)
 }
 
-fn get_content(url: String) -> Result<String, ureq::Error> {
-    Ok(if std::fs::exists(FILENAME)? {
+fn get_content(url: String, filename: &str) -> Result<String, ureq::Error> {
+    Ok(if std::fs::exists(filename)? {
         info!("parsing filename");
-        get_content_file(FILENAME, |e| error!("Error reading file: {}", e))
+        get_content_file(filename, |e| error!("Error reading file: {}", e))
     } else {
         info!("parsing web content");
         get_content_web(url)?
     })
 }
 
-fn get_content_file<C: FnOnce(std::io::Error)>(path: &str, on_error: C) -> String {
+fn get_content_file<C: FnOnce(std::io::Error)>(
+    path: &str,
+    on_error: C,
+) -> String {
     match std::fs::read_to_string(path) {
         Ok(content) => content,
         Err(error) => {
@@ -103,14 +109,14 @@ fn parse_images_from_content(content: String) -> Vec<String> {
     images_paths
 }
 
-fn process_images(host: String, images: Vec<String>) -> Result<(), ureq::Error> {
+fn process_images(host: &str, images: Vec<String>) -> Result<(), ureq::Error> {
     for (index, image_path) in images
         .iter()
         .enumerate()
     {
-        info!("{}: {}", index, image_path);       
+        info!("{}: {}", index, image_path);
 
-        let image_content = get_content_bytes(host.clone() + image_path)?;
+        let image_content = get_content_bytes(String::from(host) + image_path)?;
 
         std::fs::write(format!("img_{}.gif", index), image_content)?;
     }
@@ -138,3 +144,6 @@ fn get_images_paths(sel: Select) -> Vec<String> {
     .collect()
 }
 
+fn get_synaptic_url(host: &String) -> String {
+    format!("{}/mapsynop", host)
+}
